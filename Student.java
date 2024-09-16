aa4c8eb0bf6f4957380040d4328e25e31d198b44
NgoNhat2212 <151526374+NgoNhat2212@users.noreply.github.com>
2024-09-16 11:05:51 +0700

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,13 +1,44 @@
-package Student;
+package Homework;
 
-public class Student {
-    int ID;
-    String name;
-    public Student(int ID, String name) {
-        this.ID = ID;
-        this.name = name;
+import java.util.Date;
+
+class Student extends person implements IPerson {
+
+    private float GPA;
+    private String major;
+
+    public Student() {
     }
-    void displayInformation(){
-        System.out.println("ID: " + ID + ", Name: " + name);
+
+    public Student(float GPA, String major, String ID, String fullname, Date Date0fbrith) {
+        super(ID, fullname, Date0fbrith);
+        this.GPA = GPA;
+        this.major = major;
     }
+
+    public float getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(float GPA) {
+        this.GPA = GPA;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("ID: " + ID);
+        System.out.println("Full Name: " + fullname);
+        System.out.println("Date of birth: " + Date0fbrith);
+        System.out.println("GPA: " + GPA);
+        System.out.println("Major: " + major);
+    }
+
 }
